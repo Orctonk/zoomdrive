d8417c333e5934d6cac8eca50fe6eace6a371b49
Erika Lundin <ie16eln@cs.umu.se>
2020-05-14 16:15:38 +0200

    Acceleratorn

---
+++ remote-1/accelerator.c
@@ -0,0 +1,17 @@
+#include "accelerator.h"
+#include <util/delay.h>
+#include <stdio.h>
+
+void accInit(){
+    ADCSRA = (1<<ADEN) | (0<<ADATE) | (1<<ADPS1) | (1<<ADPS0);
+    ADMUX |= (1<<REFS0)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0)|(0<<ADLAR);
+}
+
+int accRead(){
+    ADMUX = (1<<REFS0)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0)|(0<<ADLAR);
+    ADCSRA |= (1<<ADSC);
+
+    while(bit_is_set(ADCSRA, ADSC));
+
+    return ADC;
+}
---
+++ remote-1/accelerator.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void accInit(void);
+
+int accRead(void);
