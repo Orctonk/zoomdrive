d38a938a338f08112c22c368b4a1b16ec2ebe809
Mimmi Edholm <id18mim@cs.umu.se>
2020-05-18 11:36:49 +0200

    Updated main

--- vehicle/src/main.c
+++ vehicle/src/main.c
@@ -17,6 +17,7 @@
 #include "drivers/timer.h"
 #include "drivers/summer.h"
 #include "drivers/engine.h"
+#include "drivers/extender_interface.h"
 #include "message.h"
 
 #define LED_PORT PORTC
@@ -27,6 +28,8 @@
 int32_t last_hb;         //Last recieved heartbeat.
 
 bool locked;             //Locked if in emergency stop mode.
+bool dm1;                // Deadmam's grip from controller 1
+bool dm2;                // Deadmam's grip from controller 2
 
 /*
  * Initialze device by calling all initialization functions 
@@ -41,7 +44,9 @@ void init(){
     LED_DDR|=(1<<LED_PIN);
 	LED_PORT&=~(1<<LED_PIN);
 
-    locked = false; 
+    locked = true; 
+    dm1 = false;
+    dm2 = false; 
 }
 
 /*
@@ -60,6 +65,18 @@ void callback(Message msg){
         LCD_write_string("PING PONG ");
         Message_Send(reply, 0); 
     }
+    else if(msg.type == DEADMAN){
+      if(msg.args[0] == 0){
+          dm1 = msg.args[1];
+      }
+      else if(msg.args[0] == 1){
+          dm2 = msg.args[1];
+      }
+      if(dm1 && dm2){
+          //If both controllers deadman's grip is activated the vheicle should stop.
+          engine_set_speed(0);  
+      }
+    }
     else if(msg.type == HONK){
         if(!locked) {
             summer_beep();
@@ -69,18 +86,23 @@ void callback(Message msg){
         if(msg.args[0] == 1){
             //Lock
             locked = true; 
-            engine_set_speed(0);     
+            engine_set_speed(0);   
+            LCD_set_cursor(0x00);
+            LCD_write_string("STOPPED");  
         }
-        else if(msg.args[0] == 0){
+        else if(msg .args[0] == 0){
             //Unlock
             locked = false; 
+            CD_set_cursor(0x00);
+            LCD_write_string("      "); 
         }
     }
     else if(msg.type == ENGINE_POWER){
         if(!locked) {
-            engine_set_speed(atoi(msg.args[0]));
+            if((dm1 && !dm2)||(dm2 && !dm1)){
+                engine_set_speed(atoi(msg.args[0]));
+            }
         }
-        
     }
     else if(msg.type == HEADING){
         if(!locked) {
@@ -89,6 +111,7 @@ void callback(Message msg){
        
     }
     else if(msg.type == HEARTBEAT){
+        //Save time of heartbeat. 
         last_hb = timer_get_time();
     }
 }
@@ -106,7 +129,19 @@ int main(void) {
         if (timer_get_time() - last_hb > 3){
             engine_set_speed(0);
         }
-        while(locked){
-                LED_PORT|=(1<<LED_PIN);
-            }
+        if(locked){
+            LED_PORT |= (1<<LED_PIN);
+        }
+        else {
+            LED_PORT &= ~(1<<LED_PIN);
+        }
+       if(engine_get_speed() == -1){
+           //If the vhiecle is backing, 
+           summer_start(); 
+           yellow_lamp(1); 
+       }
+       else {
+           summer_stop();
+           yellow_lamp(0); 
+       }
 }
