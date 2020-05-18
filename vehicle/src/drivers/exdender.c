4f1e6e14f8906b070d14276f9ff3846ab3e65ddd
Mimmi Edholm <id18mim@cs.umu.se>
2020-05-18 11:39:13 +0200

    Exdender interface and stuff

--- vehicle/src/drivers/exdender.c
+++ vehicle/src/drivers/exdender.c
@@ -1,2 +1,66 @@
+/*
+ * extender.c
+ * 
+ * Date: 2020-05-115
+ * 
+ * Author: Mimmi Edholm 
+ * 
+ * 
+ */
+
 #include "extender_interface.h"
+#include "i2cmaster.h"
+
+/*
+ * Initialize 
+ */
+void init(){
+    i2c_init();
+
+}
+
+//THe fuk funkar knappen?? 
+
+/*
+ * Turn on or off the green lamp.
+ * 
+ * lamp_switch: If 1, turn lamp on. If 0, turn lamp off.
+ */
+void green_lamp(bool lamp_switch){
+    if(lamp_switch){
+        if( i2c_start((SLAVE_ADRESS<<1)|I2C_WRITE ) != 0){
+			i2c_stop();
+			return 0;
+		}
+
+		if( i2c_write(2) != 0) {
+			i2c_stop();
+			return 0;
+		}
+    }
+}
+
+/*
+ * Turn on or off the green lamp.
+ * 
+ * lamp_switch: If 1, turn lamp on. If 0, turn lamp off.
+ */
+void yellow_lamp(bool lamp_switch){
+    if(lamp_switch){
+        if( i2c_start((SLAVE_ADRESS<<1)|I2C_WRITE ) != 0){
+			i2c_stop();
+			return 0;
+		}
+
+		if( i2c_write(1) != 0) {
+			i2c_stop();
+			return 0;
+		}
+    }
+}
+
+/*
+ * Return true if the distance sensors are within 15cm of an object. 
+ */
+bool within_15cm(); 
 
--- vehicle/src/drivers/extender_interface.h
+++ vehicle/src/drivers/extender_interface.h
@@ -13,10 +13,7 @@
 
 #include <stdbool.h>
 
-// #define EX_PORT PORTC
-// #define SDA_PIN PIN4
-// #define SCL_PIN PIN5
-
+#define SLAVE_ADRESS 0x07
 /*
  * Initialize 
  */
@@ -27,33 +24,19 @@ void init();
  * 
  * lamp_switch: If 1, turn lamp on. If 0, turn lamp off.
  */
-void green_lamp(int lamp_switch);
+void green_lamp(bool lamp_switch);
 
 /*
  * Turn on or off the green lamp.
  * 
  * lamp_switch: If 1, turn lamp on. If 0, turn lamp off.
  */
-void yellow_lamp(int lamp_switch); 
+void yellow_lamp(bool lamp_switch); 
 
 /*
  * Return true if the distance sensors are within 15cm of an object. 
  */
 bool within_15cm(); 
-
-/*
- * Turn on or off the right turn signal.
- * 
- * lamp_switch: If 1, turn lamp on. If 0, turn lamp off.
- */
-void turn_signal_right(int lamp_switch);
-
-/*
- * Turn on or off the left turn signal.
- * 
- * lamp_switch: If 1, turn lamp on. If 0, turn lamp off.
- */     
-void turn_signal_left(int lamp_switch); 
 
 
 #endif
