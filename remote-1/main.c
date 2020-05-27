76f97a3f7506d904437fea1458405643f19af9fc
Erika Lundin <ie16eln@cs.umu.se>
2020-05-27 16:06:42 +0200

    pins

--- remote-1/main.c
+++ remote-1/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <stdio.h>
 #include "define.h"
 #include <stdlib.h>
 #include "summer.h"
@@ -10,51 +11,35 @@
 #include <util/delay.h> 
 #include "lcd.h"
 
-/*
-   Simple program that takes the temperature of an object when it is at the correct distance
-   and the device is tilted at a 90 degree angle.
-
-   Written by Jakob Lundkvist 
-   */
-
 
 // Global variables
 static volatile int cBtn = 0;
 static volatile int lastCallback = 0;
 static volatile int callStop = 0;
-static volatile char *emStateString, *distString, *speedString;
+static volatile char *emStateString, *distString, *speedString, *cString;
 int reset = 0;
-int mH = 0;
-int strInt, prev, mPrev, mStrInt;
+int strInt, prev;
 char *currStr;
 char *selStr;
 char *nxtStr;
-char *mCurrStr, *mSelStr, *mNxtStr;
-char *mStrings[5];
-char *strings[6];
+char *strings[5];
 
 void blink(void) {
-    PORTC |= (1<<PORTC5);
+    PORTB |= (1<<PORTB6);
     _delay_ms(10);
-    PORTC &= ~(1<<PORTC5);
+    PORTB &= ~(1<<PORTB6);
     _delay_ms(10);
 }
 
 void ledInit(void) {
-    DDRC |= (1<<PINC5) | (1<<PINC4) | (1<<PINC3);
-    PORTC &= ~(1<<PINC5) | (1<<PINC4) | (1<<PINC3);
+    DDRB |= (1<<PINB6) | (1<<PINB7);
+    PORTB &= ~(1<<PINB6) | (1<<PINB7);
 }
 
 void btnInit(void) {
 
-    DDRD &= ~(1<<PIND3) | (1<<PIND6) | (1<<PIND7);
-    PORTD |= (1<<PIND3) | (1<<PIND6) | (1<<PIND7);
-}
-
-void joyStickInit(void) {
-
-    DDRC &= ~(1<<PINC2);
-    PORTC |= (1<<PINC2);
+    DDRC &= ~(1<<PINC0) | (1<<PINC1) | (1<<PINC2);
+    PORTC |= (1<<PINC0) | (1<<PINC1) | (1<<PINC2);
 }
 
 /*
@@ -94,62 +79,10 @@ int readADC(int channel) {
     return ADCW;
 }
 
-void MelmenuRight(void) {
-    _delay_ms(10);
-    if (!(++mStrInt > 4)) {
-        mCurrStr = mNxtStr;
-        mNxtStr = mStrings[mStrInt];
-    } else {
-        mStrInt = 0;
-        mCurrStr = mNxtStr;
-        mNxtStr = mStrings[mStrInt];
-    }
-}
-
-void MelmenuLeft(void) {
-
-    _delay_ms(10);
-    if (!(--mStrInt < 0)) {
-        mNxtStr = mCurrStr;
-        mCurrStr = mStrings[mStrInt];
-    } else {
-        mStrInt = 4;
-        mNxtStr = mCurrStr;
-        mCurrStr = mStrings[mStrInt];
-    }
-
-}
-
-void menuRight(void) {
-    _delay_ms(10);
-    if (!(++strInt > 5)) {
-        currStr = nxtStr;
-        nxtStr = strings[strInt];
-    } else {
-        strInt = 0;
-        currStr = nxtStr;
-        nxtStr = strings[strInt];
-    }
-}
-
-void menuLeft(void) {
-
-    _delay_ms(10);
-    if (!(--strInt < 0)) {
-        nxtStr = currStr;
-        currStr = strings[strInt];
-    } else {
-        strInt = 4;
-        nxtStr = currStr;
-        currStr = strings[strInt];
-    }
-
-}
-
 void timeCallback(void) {
 
     if (lastCallback) {
-        PORTC |= (1<<PINC4);
+        PORTB |= (1<<PINB7);
     }
     lastCallback = 1;
 }
@@ -170,11 +103,12 @@ void sendMessage(int topic, char* payLoad1, char* payLoad2, Message msg) {
 
 void callback(Message msg) {
 
+    blink();
     switch(msg.type) {
 
         case HEARTBEAT:
             lastCallback = 0;
-            PORTC &= ~(1<<PINC4);
+            PORTB &= ~(1<<PINB7);
 
             if (!strcmp(msg.args[0], "0")) {
                 Message msg;
@@ -183,8 +117,16 @@ void callback(Message msg) {
             }
             break;
 
+        case DEADMAN:
+            
+            if (!strcmp(msg.args[0], "1")) {
+                PORTC ^= (1<<PINC2);
+            }
+
+            break;
+
         case HONK:
-            if(!strcmp(msg.args[0], "1")) {
+            if(!strcmp(msg.args[0], "0")) {
                 if (!strcmp(msg.args[1], "1")) {
                     summer_start();
                 } else {
@@ -194,10 +136,11 @@ void callback(Message msg) {
 
             break;
 
-        case CARBUTTON:
+        case CSTSTRING:
+            strcpy(cString, msg.args[0]);
+            break;
 
-            // Safer - controlls correct input
-            
+        case CARBUTTON:
             if (!strcmp(msg.args[0], "1")) {
                 cBtn = 1;
                 summer_start();
@@ -205,35 +148,12 @@ void callback(Message msg) {
                 cBtn = 0;
                 summer_stop();
             }
-            
-
             break;
-
-        case UPDATE_EM:
-
-            if (!strcmp(msg.args[0], "1")) {
-                PORTC |= (1<<PINC3);
-            } else {
-                PORTC &= ~(1<<PINC3);
-            }
-            strcpy(emStateString, msg.args[0]);
-            break;
-
-        //case UPDATE:
-        //    if (!strcmp(msg.args[0], "1")) {
-        //        PORTC |= (1<<PINC3);
-        //    } else {
-        //        PORTC &= ~(1<<PINC3);
-        //    }
-        //    strcpy(emStateString, msg.args[0]);
-        //    strcpy(speedString, msg.args[1]);
-        //    strcpy(distString, msg.args[2]);
-        //    break;
 
 
         default:
             break;
-        }
+    }
 }
 
 
@@ -261,14 +181,10 @@ void horDrive(int dir) {
     }
 }
 
-void writeRegMenu(int dead, int gear) {
-    writeString(currStr);
-    writeString("  G:");
+void writeToScreen(int dead, int gear) {
+    writeString("G:");
     writeData(gear+48);
-    moveCursor(0b10010000);
-    writeString("Selected:");
-    writeString(selStr);
-    writeData(' ');
+
     if (dead) {
         writeData('D');
     } else {
@@ -276,8 +192,12 @@ void writeRegMenu(int dead, int gear) {
     }
     writeData(cBtn + 48);
 
+    moveCursor(0b10010000);
+
+    writeString("INFO: ");
+    writeString(cString);
+
     moveCursor(0b10100000);
-    
 
     writeString("EB:");
     writeString(emStateString);
@@ -285,6 +205,7 @@ void writeRegMenu(int dead, int gear) {
     writeString(speedString);
     writeString(" D:");
     writeString(distString);
+
 }
 
 
@@ -297,7 +218,6 @@ void inits(void) {
     summer_init();
     ADCInit();
     ledInit();
-    joyStickInit();
     btnInit();
     Message_Init(4800);
     Message_Register(0xff, callback);
@@ -306,14 +226,14 @@ void inits(void) {
 int checkDead(int dead) {
     Message msg;
 
-    if (!(PIND & (1<<7)) && (dead != 1)) {
+    if (!(PINC & (1<<2)) && (dead != 1)) {
 
-        sendMessage(DEADMAN, "1", "1", msg);
+        sendMessage(DEADMAN, "2", "1", msg);
         _delay_ms(50);
         return 1;
-    } else if ((PIND & (1<<7)) && (dead != 0)) {
+    } else if ((PINC & (1<<2)) && (dead != 0)) {
 
-        sendMessage(DEADMAN, "1", "0", msg);
+        sendMessage(DEADMAN, "2", "0", msg);
         _delay_ms(50);
         return 0;
     }
@@ -321,154 +241,25 @@ int checkDead(int dead) {
     return dead;
 }
 
-int cheatCodes(int cState) {
-    while (1) {
-        clearDisplay();
-        writeString("Enter Code:");
-        writeData(cState+48);
-        moveCursor(0b10010000);
-        writeString("EXIT=6&7");
-
-        if (!cState && !(PIND & (1<<7))) {
-            cState++;
-        } else if ((cState == 1) && (!(PIND & (1<<6)))){
-            cState++;
-        } else if ((cState == 2) && (!(PIND & (1<<3)))){
-            Message msg;
-            sendMessage(HONK, "3", NULL, msg);
-            break;
-        } else if (!(PIND & (1<<7)) && !(PIND & (1<<6))) {
-            return 0;
-        }
-        _delay_ms(50);
-
-    }
-    return 0;
-}
-
-int getVert() {
-
-
-    int vertAdc = readADC(0);
-
-    if (vertAdc >= 800) {
-        return 1;
-    } else if (vertAdc <= 400) {
-        return -1;
-    } else {
-        return 0;
-    }
-
-}
-
-int getHor() {
-
-    int horAdc = readADC(1);
-
-    if (horAdc >= 800) {
-        return 1;
-    } else if (horAdc <= 400) {
-        return -1;
-    } else {
-        return 0;
-    }
-
-}
-
-void writeMelMenu() {
-
-    writeString(mCurrStr);
-    writeString(" ");
-    writeString(mNxtStr);
-    moveCursor(0b10010000);
-    writeString("Selected: ");
-    writeString(mSelStr);
-    moveCursor(0b10100000);
-    writeData(mStrInt + 48);
-}
-
-void melodiesMenu() {
-
-    while(1) {
-        clearDisplay();
-        writeMelMenu();
-
-        mH = getHor();
-
-        if (mH == -1) {
-
-            MelmenuRight();
-            if (mPrev) {
-                MelmenuRight();
-            }
-            mPrev = 0;
-
-        } else if (mH == 1) {
-
-            MelmenuLeft();
-            if (!mPrev) {
-                MelmenuLeft();
-            }
-            mPrev = 1;
-        } 
-
-        if (!(PINC & (1<<2))) {
-            mSelStr = mCurrStr;
-            if (!strcmp(mSelStr, "EXIT")) {
-                return;
-            } else {
-                char buff[10];
-                Message msg;
-                sendMessage(HONK, "2", itoa(mStrInt, buff, 10), msg);
-            }
-        }
-
-        _delay_ms(50);
-
-    }
-
-}
-
 /*
    The main loop that controlls the program flow. 
    */
 int main(void) {
 
-    strInt = prev = -1;
-    mStrInt = 0;
-    mPrev = 1;
-
-    mStrings[0] = "Glass";
-    mStrings[1] = "USSR";
-    mStrings[2] = "HG";
-    mStrings[3] = "Twinkle";
-    mStrings[4] = "EXIT";
-
-    mSelStr = mCurrStr = mStrings[0];
-    nxtStr = mStrings[1];
-
-    strings[0] = "st1";
-    strings[1] = "st2";
-    strings[2] = "st2";
-    strings[3] = "st3";
-    strings[4] = "Melodies";
-    strings[5] = "CC";
-
-    selStr = currStr = strings[0];
-    nxtStr = strings[1];
+    cString = "NaN";
     emStateString = "E";
     speedString = "S";
     distString = "D";
 
-
-    int cState, dead, vertAdc, horAdc, ldh, lndh, lH, lV, vert, hor, gear;
-    cState = dead = vertAdc = horAdc = lH = lV = ldh = lndh = vert = hor = 0;
+    int dead, vertAdc, horAdc, ldh, lndh, lH, lV, vert, hor, gear;
+    dead = vertAdc = horAdc = lH = lV = ldh = lndh = vert = hor = 0;
     gear = 1;
 
     Message msg;
 
     inits();
     sei();
+
 
 
     // Loop as long as there is power in the MCU.
@@ -479,55 +270,61 @@ int main(void) {
 
         clearDisplay();
 
-        vert = getVert();
-        hor = getHor();
+        vertAdc = readADC(0);
+        horAdc = readADC(1);
+
+
+
+        if (vertAdc >= 530) {
+            vert = 1;
+        } else if (vertAdc <= 470) {
+            vert = -1;
+        } else {
+            vert = 0;
+        }
+
+
+        // Kanke vänd på dessa???
+        /*
+         * Kanske sova.....
+         *
+         * */
+        if (horAdc >= 530) {
+            hor = 1;
+        } else if (horAdc <= 470) {
+            hor = -1;
+        } else {
+            hor = 0;
+        }
 
         if ((dead = checkDead(dead))) {
 
-            if (!(PIND & (1<<6)) && (ldh != 1)) {
+            if (!(PINC & (1<<1)) && (ldh != 1)) {
                 sendMessage(HONK, "2", "1", msg);
                 ldh = 1;
-            } else if ((PIND & (1<<6)) && (ldh != 0)) {
+            } else if ((PINC & (1<<1)) && (ldh != 0)) {
                 sendMessage(HONK, "2", "0", msg);
                 ldh = 0;
-            }  
-
-            if (lV != vert) {
+            } else if (lV != vert) {
                 lV = vert;
                 vertDrive(vert);
-            }
-
-            if (lH != hor) {
+            } else if (lH != hor) {
                 lH = hor;
                 horDrive(hor);
             } 
 
         } else {
 
-            if (hor == -1) {
-
-                menuRight();
-                if (prev) {
-                    menuRight();
-                }
-                prev = 0;
-
-            } else if (hor == 1) {
-
-                menuLeft();
-                if (!prev) {
-                    menuLeft();
-                }
-                prev = 1;
-            } 
-
-            if (!(PIND & (1<<6)) && (lndh != 1)) {
-                sendMessage(HONK, "0", "1", msg);
+            /*
+             * End me 
+             * */
+            if (!(PINC & (1<<1)) && (lndh != 1)) {
+                sendMessage(HONK, "1", "1", msg);
                 lndh = 1;
-            } else if ((PIND & (1<<6)) && (lndh != 0)) {
-                sendMessage(HONK, "0", "0", msg);
+            } else if ((PINC & (1<<1)) && (lndh != 0)) {
+                sendMessage(HONK, "1", "0", msg);
                 lndh = 0;
-            } else if (!(PIND & (1<<3))) {
+            } else if (!(PINC & (1<<0))) {
 
                 switch (++gear) {
                     case 2:
@@ -542,22 +339,10 @@ int main(void) {
                 }
                 _delay_ms(30);
 
-            } else if (!(PINC & (1<<2))) {
-
-                if (!strcmp(currStr, "CC")) {
-                    cState = cheatCodes(cState);
-                } else if (!strcmp(currStr, "Melodies")) {
-                    _delay_ms(30);
-                    melodiesMenu();
-
-                } else {
-                    selStr = currStr;
-                    sendMessage(CSTSTRING, selStr, NULL, msg);
-                }
-            }
+            } 
         }
 
-        writeRegMenu(dead, gear);
+        writeToScreen(dead, gear);
 
         _delay_ms(50);
     }
